d46fa42e398b861c573edda2e97ca4a50703d8d5
Kaue-G <kaueguedes.dev@gmail.com>
2022-09-30 12:02:02 -0300

    global styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+    white: "#fff",
+
+    primary: "#25294C",
+    secondary: "#202342",
+    textPrimary: "#333333",
+    textSecondary: "#E9ECEF",
+    background: "#E9ECEF",
+    positive: "#0D6EFD",
+    success: "#198754",
+    warning: "#FFC107",
+    danger: "#DC3545",
+    brand: "#27C498",
+};
