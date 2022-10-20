a1954088eeb0ff347183bb08b819a3ac30e19d4c
Kaue-G <kaueguedes.dev@gmail.com>
2022-10-20 16:10:14 -0300

    styles task card

--- src/pages/Home/pageComponentes/CardTask/intex.tsx
+++ src/pages/Home/pageComponentes/CardTask/intex.tsx
@@ -1,13 +1,34 @@
-import { CardContainer, ContentsTask, StatusTask, TaskFooter } from "./styles";
+import { CardContainer, ContentsTask, OptionsBtn, StatusTask, TaskFooter } from "./styles";
+import { AiOutlineUnorderedList } from "react-icons/ai";
+import { BiTimeFive } from "react-icons/bi";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 export function CardTask() {
     return (
         <CardContainer>
             <StatusTask>
-
+                Status
             </StatusTask>
-            <ContentsTask></ContentsTask>
-            <TaskFooter></TaskFooter>
+            <ContentsTask>
+                <div>
+                    <span />
+                    <div className="TittleAndDescription">
+                        <h1>Titulo task</h1>
+                        <p>Descrição task</p>
+                    </div>
+                </div>
+                <div>
+                    <OptionsBtn>
+                        <BsThreeDotsVertical size={"10px"} />
+                    </OptionsBtn>
+                </div>
+            </ContentsTask>
+            <TaskFooter>
+                <AiOutlineUnorderedList />
+                <span>tarefas</span>
+                <BiTimeFive />
+                <span>00 de mes, 00:00</span>
+            </TaskFooter>
         </CardContainer>
     );
 };
--- src/pages/Home/pageComponentes/CardTask/styles.ts
+++ src/pages/Home/pageComponentes/CardTask/styles.ts
@@ -15,13 +15,64 @@ export const CardContainer = styled.div`
 `;
 
 export const StatusTask = styled.h1`
-    
+    font-size: 16px;
+    font-weight: 500;
+    margin-left: 4px;
+    margin-bottom: 5px;
 `;
 
 export const ContentsTask = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
+    padding-top: 7px;
+    border-top: solid 1px ${(props) => props.theme['background']};
+
+    span {
+        display: flex;
+        width: 2px;
+        height: 32px;
+        background: blue;
+    }
+
+    div:first-child {
+        display: flex;
+        gap: 10px;
+
+        h1 {
+            font-size: 16px;
+            font-weight: 700;
+            color: ${(props) => props.theme['textPrimary']};
+        }
+        p {
+            font-size: 10px;
+            font-weight: 500;
+            color: ${(props) => props.theme['textSecondary']};
+        }
+    }
+`;
+
+export const OptionsBtn = styled.button`
+    display: flex;
+    flex-direction: column;
+    color: ${(props) => props.theme['textPrimary']};
+
+    background: transparent;
+    border: none;
+    cursor: pointer;
 `;
 
 export const TaskFooter = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    margin-top: 9.5px;
+    svg {
+        color: ${(props) => props.theme['textSecondary']};
+    }
     
+    span {
+        font-size: 9px;
+        font-weight: 700;
+        color: ${(props) => props.theme['textPrimary']};
+    }
 `;
